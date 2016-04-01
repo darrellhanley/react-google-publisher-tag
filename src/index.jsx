b43aea793262e4bf3db0e8237481bdb6b2d6214a
Darrell Hanley <contact@darrellhanley.com>
2016-04-01 14:14:55 -0400

    Revert "feat: adds support for slot level targeting"

--- src/index.jsx
+++ src/index.jsx
@@ -95,7 +95,6 @@ export default class GooglePublisherTag extends Component {
 
     sizeMap: React.PropTypes.array,
     dimensions: React.PropTypes.array,  // [[300, 600], [160, 600]]
-    targeting: React.PropTypes.object, // {key: value}
 
     minWindowWidth: React.PropTypes.number.isRequired,
     maxWindowWidth: React.PropTypes.number.isRequired,
@@ -201,14 +200,6 @@ export default class GooglePublisherTag extends Component {
 
       if(props.sizeMap) {
         slot.defineSizeMapping(props.sizeMap);
-      }
-
-      if (props.targeting) {
-        for (const key in props.targeting) {
-          if ({}.hasOwnProperty.call(props.targeting, key)) {
-            slot.setTargeting(key, props.targeting[key]);
-          }
-        }
       }
 
       // display new slot
